c3173be6f3eb56d9fdf6a62e63280b01c9463f99
Traveler3114 <matej.krizic16@gmail.com>
2026-03-16 12:07:05 +0100

    feat: add custom bottom navigation bar component and GTFS route data transfer object

--- GetThereShared/Dtos/GtfsRouteDto.cs
+++ GetThereShared/Dtos/GtfsRouteDto.cs
@@ -5,7 +5,7 @@ namespace GetThereShared.Dtos
         public string RouteId { get; set; } = string.Empty;
         public string ShortName { get; set; } = string.Empty;
         public string LongName { get; set; } = string.Empty;
-        public string Color { get; set; } = "1a73e8";
+        public string? Color { get; set; } = "1a73e8";
         public int RouteType { get; set; }
         public List<double[]> Shape { get; set; } = new();
     }
